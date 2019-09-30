ff36e314bb744393de35add9223c1c0f2fbd8f40
FYLSunghwan <sunghwan519@hotmail.com>
2019-09-30 14:49:39 +0900

    refactor: hw4-2 Move functions into class "Item"

--- hw4-2/my_list.cc
+++ hw4-2/my_list.cc
@@ -44,69 +44,98 @@ StrArray split(const std::string& str, const std::string& delim) {
     return StrArray(string_list, idx+1);
 }
 
-struct Item {
-    std::string name;
-    int age;
-    std::string id;
-    std::vector<std::string> subjects;
-    Item* next = nullptr;
-};
-
-struct Item* create_item() {
-// 하나의 struct Item 형 변수를 동적할당한 후 이에 대한 pointer 를 리턴한다.
-    Item* newItem = new Item;
-    return newItem;
-}
-
-void insert_item(struct Item *prev_item, struct Item *item) {
-// head 에 포함되어 있는 prev_item 바로 뒤에 새로운 item 을 추가한다.
-    prev_item->next = item;
-}
-
-int main(void){ 
-    std::ifstream fin;
-    fin.open("input.txt");
-    std::string buffer;
-    Item* head = nullptr;
-    Item* node = nullptr;
-    int sw=0;
-    while (fin.peek() != EOF) {
-        Item* item = create_item();
-        if(!sw) {
-            node = head = item;
-            sw=1;
+class Item {
+    public:
+        Item() : m_age(0), m_subsize(0), m_next(nullptr) {
+            // Do Nothing
         }
-        getline(fin, buffer);
-        StrArray inpStr = split(buffer,":");
-        item->name = inpStr[0];
-        item->age = stoi(inpStr[1]);
-        item->id = inpStr[2];
-        StrArray subjects_ = split(inpStr[3],",");
-        for(int i=0;i<subjects_.size();i++)
-        {
-            item->subjects.push_back(subjects_[i]);
+
+        ~Item() {
+            if(m_next != nullptr) {
+                delete m_next;
+            }
         }
-        insert_item(node, item);
-        node = node->next;
-    }
-    
-    while(head!=nullptr)
-    {
-        if(head->id.substr(0,4) == "2013") {
-            std::cout<<head->name;
-            std::cout<<"("<<head->id<<") : ";
-            for(int i=0;i<head->subjects.size();i++)
+
+        Item(const std::string &str) : m_next(nullptr) {
+            StrArray inpStr = split(str,":");
+            m_name = inpStr[0];
+            m_age = stoi(inpStr[1]);
+            m_id = inpStr[2];
+            StrArray subjects_ = split(inpStr[3],",");
+            for(int i=0;i<subjects_.size();i++)
             {
-                std::string subject = head->subjects[i];
+                m_subjects.push_back(subjects_[i]);
+            }
+        }
+
+        Item* create_item() {
+            // 하나의 struct Item 형 변수를 동적할당한 후 이에 대한 pointer 를 리턴한다.
+            Item* newItem = new Item;
+            return newItem;
+        }
+
+        void insert_item(Item *item) {
+            // head 에 포함되어 있는 prev_item 바로 뒤에 새로운 item 을 추가한다.
+            m_next = item;
+        }
+
+        void PrintSubjects() const {
+            for(int i=0;i<m_subjects.size();i++)
+            {
+                std::string subject = m_subjects[i];
                 if(i) std::cout <<" & ";
                 std::cout<<subject;
             }
             std::cout << std::endl;
         }
-        Item* tmp = head->next;
-        if(head!= nullptr) delete head;
-        head = tmp;
+        
+        const std::string& GetName() const {
+            return m_name;
+        }
+
+        const std::string& GetID() const {
+            return m_id;
+        }
+
+        Item* GetNextNode() const {
+            return m_next;
+        }
+
+    private:
+        std::string m_name;
+        int m_age;
+        std::string m_id;
+        std::vector<std::string> m_subjects;
+        size_t m_subsize;
+        Item* m_next;
+};
+
+int main(void){ 
+    std::ifstream fin;
+    fin.open("input.txt");
+    std::string buffer;
+    Item tmphead;
+    Item* node = &tmphead;
+    int sw=0;
+    while (fin.peek() != EOF) {
+        getline(fin, buffer);
+        Item* item = new Item(buffer);
+        
+        node->insert_item(item);
+        node = item;
     }
     fin.close();
+    
+    Item* head = tmphead.GetNextNode();
+
+    while(head!=nullptr)
+    {
+        if(head->GetID().substr(0,4) == "2013") {
+            std::cout<<head->GetName();
+            std::cout<<"("<<head->GetID()<<") : ";
+            head->PrintSubjects();
+        }
+        head = head->GetNextNode();
+    }
     return 0;
 }
